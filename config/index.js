7e51ae9cf20c13e5e4386297610bb96471e7ae08
lakshmivh <lakshmi.vh@gmail.com>
2023-02-03 13:04:26 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ var configValues = require("./config");
 
 module.exports = {
     getDbConnectionString: function () {
-        return `mongodb://${configValues.username}:${configValues.password}@${configValues.hostname}/${configValues.database}`;
+        //return `mongodb://${configValues.username}:${configValues.password}@${configValues.hostname}/${configValues.database}`;
+        return `mongodb://${process.env.DB_USER_NAME}:${configValues.DB_USER_PWD}@${configValues.DB_HOST}/${configValues.DB_NAME}`;
     }
 }
